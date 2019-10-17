3609b55bc15edbb58f027e7f7ebcfcd34193f2f6
austineblaise <austineblaise@gmail.com>
2019-10-16 21:58:32 -0700

    i just want to commit

---
+++ script.js
@@ -0,0 +1,41 @@
+document.getElementById("beast").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>Beauty and the Beast</h1><p>Genre: Fantancy</p><p>Author: Nani</p><p><ul><li>beauty and the beast</li></u></p>"
+}
+
+document.getElementById("know").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>You Don't know Js</h1><p><strong>Genre:</strong>Programming</p><p><strong>Author</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+
+document.getElementById("laravel").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>You Don't know Laravel</h1><p><strong>Genre:</strong>Programming</p><p><strong>Author</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+
+document.getElementById("kat").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>Katzing</h1><p><strong>Genre:</strong>Programming</p><p><strong>Author:</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+document.getElementById("guide").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>Graph QL</h1><p><strong>Genre:</strong>Programming</p><p><strong>Author:</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+document.getElementById("nkt").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>NTK</h1><p><strong>Genre:</strong>Programming</p><p><strong>Author:</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+
+document.getElementById("up").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>Start-up learn</h1><p><strong>Genre:</strong>Oho</p><p><strong>Author:</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+
+document.getElementById("learn").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>Learning GQL</h1><p><strong>Genre:</strong>Dev</p><p><strong>Author:</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+
+document.getElementById("game").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>Game of Thrones</h1><p><strong>Genre:</strong>Movie</p><p><strong>Author:</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+
+document.getElementById("mular").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>Mular</h1><p><strong>Genre:</strong>Movie</p><p><strong>Author:</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
+
+document.getElementById("you").onclick = function(){
+    document.getElementById("cp").innerHTML="<h1>You</h1><p><strong>Genre:</strong>Bb</p><p><strong>Author:</strong>Nani</P> <p>All Books by this Author:</p><ul><li>you don't know js</li><li>You don't know Laravel</li><li>Katzing</li><li>Graph QL Guide</li><li>Start-up learn</li><li>Mulan</li><li>Book</li><li>Book</li><li>Book</li></ul>"
+}
